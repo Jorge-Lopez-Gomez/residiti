a577007ce631b8966335980b160bfd20a5a8f92f
Jorge-Lopez-Gomez <jorfc.-@hotmail.com>
2021-11-27 12:47:21 -0600

    realacion

--- app/Models/publicofert.php
+++ app/Models/publicofert.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class publicofert extends Model
 {
     use HasFactory;
+    protected $fillable = ['name' , 'slug','descriptions', 'extract', 'price', 'image', 'visible', 'categoria_id'];
+    public function publicofert(){
+        return $this->belongsTo('App\Models\publicofert');
+    }
 }
